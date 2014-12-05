3596819394badcdbb90a562e874edebec1318a06
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-05 16:06:25 +0800

    添加gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,19 +13,44 @@ var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
 var del = require('del');
 
-var root = 'public/'; // 主目录
+/**
+ * 主目录
+ */
+var root = 'public/';
 
 /**
  * 清空build目录
  */
 gulp.task('clean', function (cb) {
-    del('build', cb);
+    del(root + 'build', cb);
 });
 
+/**
+ * 复制资源文件
+ */
+gulp.task('copy-assets', function () {
+    gulp.src(root + 'src/assets/**/*')
+        .pipe(gulp.dest(root + 'build'));
+});
+
+/**
+ * 复制,压缩 html模板文件
+ */
+gulp.task('copy-templates', function () {
+    gulp.src(root + 'src/app/**/*.html')
+        .pipe(usemin({
+            html: [minifyHtml({empty: true, quotes: true})]
+        }))
+        .pipe(gulp.dest(root + 'build'));
+});
+
+/**
+ * 压缩首页, 会压缩引用的css, js
+ */
 gulp.task('usemin', function () {
     return gulp.src(root + 'src/index.html')
         .pipe(usemin({
-            css : [minifyCss(), 'concat', rev()],
+            css : [minifyCss(), rev()],
             html: [minifyHtml({empty: true, quotes: true})],
             js  : [uglify(), rev()]
         }))
@@ -33,9 +58,9 @@ gulp.task('usemin', function () {
 });
 
 /**
- * 默认任务 :
+ * 默认任务 : 依次执行 下面任务
  * 使用 命令行: gulp运行
  */
-gulp.task('default', function () {
-    console.log('run default...');
+gulp.task('default', function (cb) {
+    runSequence('clean', ['usemin', 'copy-templates', 'copy-assets'], cb);
 });
