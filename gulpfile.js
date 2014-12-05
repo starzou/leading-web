ac21e4b9c67c7006fb3e09fb28e693ce03880c09
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-05 15:17:56 +0800

    添加gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/**
+ *@class gulpfile.js
+ *@description gulp 配置文件
+ *@time 2014-11-28 10:12
+ *@author StarZou
+ **/
+
+var gulp = require('gulp');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
+var root = 'public/';
+
+gulp.task('usemin', function () {
+    return gulp.src(root + 'src/index.html')
+        .pipe(usemin({
+            css : [minifyCss(), 'concat'],
+            html: [minifyHtml({empty: true})],
+            js  : [uglify(), rev()]
+        }))
+        .pipe(gulp.dest(root + 'build/'));
+});
+
+/**
+ * 默认任务 :
+ * 使用 命令行: gulp运行
+ */
+gulp.task('default', function () {
+    console.log('run default...');
+});
