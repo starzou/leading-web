a538176b4af03b841e4d8ceb3f8e4cb784a14017
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-08 18:04:29 +0800

    添加content

--- public/src/common/directives/pages/container/content/content.js
+++ public/src/common/directives/pages/container/content/content.js
@@ -1,19 +1,19 @@
 /**
- * @class container
- * @description 首页 container主体模块
+ * @class content
+ * @description content模块
  * @time 2014-12-05 14:20
  * @author StarZou
  **/
 (function (window, document) {
     'use strict';
 
-    var container = angular.module('directives.pages.container', ['directives.pages.container.sidebar', 'directives.pages.container.content']);
+    var content = angular.module('directives.pages.container.content', []);
 
-    container.directive('pageContainer', [function () {
+    content.directive('pageContent', [function () {
         return {
             restrict   : 'A',
             replace    : true,
-            templateUrl: 'common/directives/pages/container/container.tpl.html',
+            templateUrl: 'common/directives/pages/container/content/content.tpl.html',
             compile    : function ($element, $attr) {
                 return function ($scope, $element, $attr) {
 
