ad8a32539cfb522581e4edfd611a03abea048ce9
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-08 17:55:05 +0800

    添加sidebar

--- public/src/common/directives/pages/container/sidebar/sidebar.js
+++ public/src/common/directives/pages/container/sidebar/sidebar.js
@@ -1,19 +1,19 @@
 /**
- * @class container
- * @description 首页 container主体模块
+ * @class sidebar
+ * @description sidebar模块
  * @time 2014-12-05 14:20
  * @author StarZou
  **/
 (function (window, document) {
     'use strict';
 
-    var container = angular.module('directives.pages.container', ['directives.pages.container.sidebar']);
+    var sidebar = angular.module('directives.pages.container.sidebar', []);
 
-    container.directive('pageContainer', [function () {
+    sidebar.directive('pageSidebar', [function () {
         return {
             restrict   : 'A',
             replace    : true,
-            templateUrl: 'common/directives/pages/container/container.tpl.html',
+            templateUrl: 'common/directives/pages/container/sidebar/sidebar.tpl.html',
             compile    : function ($element, $attr) {
                 return function ($scope, $element, $attr) {
 
