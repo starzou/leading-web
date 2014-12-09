296247b1152af1983ae3eb8d95d5abaa5aa56788
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-09 13:13:26 +0800

    菜单

--- public/src/common/directives/pages/container/sidebar/sidebar.js
+++ public/src/common/directives/pages/container/sidebar/sidebar.js
@@ -16,7 +16,30 @@
             templateUrl: 'common/directives/pages/container/sidebar/sidebar.tpl.html',
             compile    : function ($element, $attr) {
                 return function ($scope, $element, $attr) {
+                    var $body = angular.element(document.body),
+                        element = $element[0],
+                        $sidebarMenu = angular.element(element.querySelector('.page-sidebar-menu')),
+                        sidebarToggler = element.querySelector('.sidebar-toggler');
 
+
+                    sidebarToggler.addEventListener('click', function () {
+                        /**
+                         * sidebar 已经关闭, 则显示
+                         */
+                        if ($body.hasClass('page-sidebar-closed')) {
+                            $body.removeClass('page-sidebar-closed');
+                            $sidebarMenu.removeClass('page-sidebar-menu-closed');
+
+                            $body.addClass('page-sidebar-fixed');
+                        } else {
+                            $body.addClass('page-sidebar-closed');
+                            $sidebarMenu.addClass('page-sidebar-menu-closed');
+
+                            $body.removeClass('page-sidebar-fixed');
+                        }
+                    });
+
+                    console.log(document.body);
                 };
             }
         }
