e2ca9223e0824e390a4702eb3e44eb9eaa11d6ac
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-10 18:11:29 +0800

    添加菜单

--- public/src/common/components/container/sidebar/menu/menu.js
+++ public/src/common/components/container/sidebar/menu/menu.js
@@ -9,14 +9,63 @@
 
     var menu = angular.module('components.container.sidebar.menu', []);
 
-    menu.directive('sidebarMenu', [function () {
-        return {
-            restrict   : 'A',
-            replace    : true,
-            templateUrl: 'common/components/container/sidebar/menu/menu.tpl.html',
-            compile    : function ($element, $attr) {
-                return function ($scope, $element, $attr) {
+    menu.provider('menuUtil', function () {
+        var html = '',
+            hasNext = false;
 
+        var me = {
+            createMenuElement: function (menus) {
+                html = '';
+                return this.renderMenuTree(menus);
+            },
+
+            /**
+             * 渲染 菜单树
+             * @param menus
+             * @returns  html
+             */
+            renderMenuTree: function (menus) {
+                menus.forEach(function (element) {
+                    hasNext = element.subList && element.subList.length > 0; // 有子菜单
+
+                    html += '<li><a href="' + element.url + '"><i class="icon-home"></i><span class="title">' + element.menuName + '</span><span class="selected"></span>';
+
+                    if (hasNext) {
+                        html += '<span class="arrow"></span>'; // 加上箭头
+                    }
+
+                    html += '</a>';
+
+                    if (hasNext) { // 渲染子菜单
+                        html += '<ul class="sub-menu">';
+                        me.renderMenuTree(element.subList);
+                        html += '</ul>';
+                    }
+
+                    html += '</li>';
+                });
+                return html;
+            }
+        };
+
+        this.$get = [function () {
+            return me;
+        }];
+    });
+
+    menu.directive('sidebarMenu', ['menuUtil', function (menuUtil) {
+        return {
+            restrict: 'A',
+            compile : function ($element, $attr) {
+                console.log('compile', $element);
+                return function ($scope, $element, $attr) {
+                    console.log('link', $element, $scope);
+
+                    var html = menuUtil.createMenuElement($scope.menus);
+                    $element[0].outerHTML = html;
+
+                    //console.log(html);
+                    //console.log($element[0]);
                 };
             }
         }
