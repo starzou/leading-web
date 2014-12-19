ebb0c3dbb95d36a916f572ba49c818c2aa14f4e6
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-19 10:10:42 +0800

    错误拦截器

--- public/src/common/services/config.js
+++ public/src/common/services/config.js
@@ -9,9 +9,47 @@
 
     var configModule = angular.module('services.config', []);
 
+    /**
+     * $http 全局配置
+     */
     configModule.config(['$httpProvider', '$cacheFactoryProvider', function ($httpProvider, $cacheFactoryProvider) {
+        /**
+         * 配置缓存
+         */
         var cacheFactory = $cacheFactoryProvider.$get();
         $httpProvider.defaults.cache = cacheFactory('cacheRequests', {capacity: 5});
+
+        /**
+         * 配置错误提示拦截器
+         */
+        $httpProvider.interceptors.push('errorInterceptor');
     }]);
+
+    /**
+     * 错误拦截器
+     */
+    configModule.provider('errorInterceptor', function () {
+        this.$get = ['$q', function ($q) {
+            return {
+                'request': function (config) {
+                    return config;
+                },
+
+                'requestError': function (rejection) {
+                    console.log('请求错误');
+                    return $q.reject(rejection);
+                },
+
+                'response': function (response) {
+                    return response;
+                },
+
+                'responseError': function (rejection) {
+                    console.log('响应错误');
+                    return $q.reject(rejection);
+                }
+            };
+        }];
+    });
 
 })(window, document);
