6f9b3d1d5b55aec14e9e40c2e07c520230dad510
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-23 18:18:00 +0800

    文件上传

--- public/src/common/components/fileUpload/fileUpload.js
+++ public/src/common/components/fileUpload/fileUpload.js
@@ -9,7 +9,35 @@
 
     var fileUpload = angular.module('components.fileUpload', []);
 
-    fileUpload.directive('fileUpload', [function () {
+    /**
+     * 上传服务对象
+     */
+    fileUpload.provider('fileUploader', function () {
+        var me = {
+            sendFile: function (options, file) {
+                var xhr = new XMLHttpRequest();
+                var formData = new FormData();
+                xhr.open("POST", options.url, true);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4) {
+                        if (xhr.status == 200) {
+                            options.success && options.success();
+                        } else {
+                            options.error && options.error();
+                        }
+                    }
+                };
+                formData.append(options.name, file);
+                xhr.send(formData);
+            }
+        };
+
+        this.$get = [function () {
+            return me;
+        }];
+    });
+
+    fileUpload.directive('fileUpload', ['fileUploader', function (fileUploader) {
         return {
             restrict   : 'A',
             replace    : true,
@@ -24,6 +52,11 @@
                         cancelUploadFilesButton = fileUploadElement.querySelector('.cancelUploadFilesButton');
 
                     $scope.files = [];
+
+                    var options = {
+                        url : '/rest/files',
+                        name: 'myFile'
+                    };
 
                     // 选择文件
                     fileInput.addEventListener('change', function () {
@@ -40,7 +73,9 @@
 
                     // 开始上传
                     startUploadFilesButton.addEventListener('click', function () {
-
+                        angular.forEach($scope.files, function (file) {
+                            $scope.upload(file);
+                        });
                     }, false);
 
                     // 取消上传
@@ -49,6 +84,15 @@
                         fileInput.files.length = 0;
                         $scope.$digest();
                     }, false);
+
+
+                    $scope.upload = function (file, index) {
+                        fileUploader.sendFile(options, file);
+                    };
+
+                    $scope.cancel = function (file, index) {
+                        $scope.files.splice(index, 1);
+                    };
 
 
                     console.log('link...');
