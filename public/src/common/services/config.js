1eaf244572b0c545dbe53a705aa1ad8cfd214c24
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-12-31 11:38:40 +0800

    全局的异常处理 alert 控件

--- public/src/common/services/config.js
+++ public/src/common/services/config.js
@@ -25,11 +25,64 @@
         $httpProvider.interceptors.push('errorInterceptor');
     }]);
 
+    configModule.provider('errorAlert', function () {
+
+        // 默认配置
+        var defaults = this.defaults = {
+            template: '<div class="alert top-right fade-in-up alert-danger" style="display: block;"><strong>_title_</strong>&nbsp;<span>_content_</span></div>',
+            show    : true,
+            duration: 3
+        };
+
+        // errorAlert控件
+        this.$get = ['$timeout', function ($timeout) {
+            var body = document.body,
+                $body = angular.element(body);
+
+            function Factory(config) {
+                var errorAlert = {},
+                    element,
+                    options = angular.extend({}, defaults, config),
+                    template = options.template;
+
+                // 替换要显示的消息
+                template = template.replace('_title_', options.title || '');
+                template = template.replace('_content_', options.content || '');
+
+                // 显示 errorAlert
+                errorAlert.show = function () {
+                    element = angular.element(template);
+                    $body.after(element);
+
+                    if (options.duration) {
+                        $timeout(function () {
+                            errorAlert.hide();
+                        }, options.duration * 1000);
+                    }
+                };
+
+                // 移除 errorAlert
+                errorAlert.hide = function () {
+                    element.remove();
+                };
+
+                // show配置
+                if (options.show) {
+                    errorAlert.show();
+                }
+
+                return errorAlert;
+            }
+
+            return Factory;
+        }];
+    });
+
     /**
      * 错误拦截器
      */
     configModule.provider('errorInterceptor', function () {
-        this.$get = ['$q', 'spinner', function ($q, spinner) {
+        this.$get = ['$q', 'spinner', 'errorAlert', function ($q, spinner, errorAlert) {
             return {
                 'request': function (config) {
                     spinner.show();
